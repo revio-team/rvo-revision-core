33715bf5cafe546d3f9b54debb61446d0d99a3c9
Sofia Lunkes <sofia.big@live.com>
2022-06-23 21:32:04 -0300

    Changed programming language from Java to Kotlin

---
+++ src/main/kotlin/com/sofia/revio/RevioApplication.kt
@@ -0,0 +1,11 @@
+package com.sofia.revio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RevioApplication
+
+fun main(args: Array<String>) {
+	runApplication<RevioApplication>(*args)
+}
---
+++ src/test/kotlin/com/sofia/revio/RevioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sofia.revio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RevioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
